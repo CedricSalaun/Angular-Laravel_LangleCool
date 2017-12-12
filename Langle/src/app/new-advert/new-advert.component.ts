cd55fd0c4d8809e2ef291161df22effaafde3ff5
CedricSalaun <salaun.cedric@gmail.com>
2017-12-12 17:37:28 +0100

    add derniere page Angular

---
+++ Langle/src/app/new-advert/new-advert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-advert',
+  templateUrl: './new-advert.component.html',
+  styleUrls: ['./new-advert.component.css']
+})
+export class NewAdvertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
